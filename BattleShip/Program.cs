ad68578f3690d523b2d4d599cfc46975e15bb017
shafin321 <shafinhaque91@gmail.com>
2020-08-20 12:19:08 +0200

    First Commit

---
+++ BattleShip/Program.cs
@@ -0,0 +1,21 @@
+﻿using BattleShipLiteLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static PlayerInfoModel CreatePlayer()
+        {
+        
+        }
+    }
+}
---
+++ BattleShipLiteLibrary/Models/PlayerInfoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipLiteLibrary.Models
+{
+  public  class PlayerInfoModel
+    {
+        public string UserName { get; set; }
+        public List<GridSpotModel> ShipLocation { get; set; }
+        public List<GridSpotModel> ShotGrid { get; set; }
+
+    }
+}
