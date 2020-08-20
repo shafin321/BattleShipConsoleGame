52b8a31754046a8d34410f0f09139321eb51ae97
shafin321 <shafinhaque91@gmail.com>
2020-08-20 13:34:59 +0200

    Store Ship-consoles

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -1,4 +1,5 @@
-﻿using BattleShipLiteLibrary.Models;
+﻿using BattleShipLiteLibrary;
+using BattleShipLiteLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,41 @@ namespace BattleShip
 
         private static PlayerInfoModel CreatePlayer()
         {
+            PlayerInfoModel player = new PlayerInfoModel();
+            //Ask the User for their name
+            player.UserName = GetUserName();
+
+            //Load the shot grid
+            GameLogic.InitilizerGrid(player);
+
+            //Ask user for their 5 ship placements
+            PlaceShips(player);
+            //Clear
+            Console.Clear();
+
+            return player;
+
+            
         
+        }
+
+        private static string GetUserName()
+        {
+            Console.WriteLine("Enter your Name");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        private static void PlaceShips(PlayerInfoModel model)
+        {
+            do
+            {
+                Console.WriteLine($"Where do you want to place the ship?{model.ShipLocation.Count +1}:");
+                string location = Console.ReadLine();
+                bool isValidLocation = GameLogic.StoreShip(model,location);// Taking location from the user and model object
+
+            } while (model.ShipLocation.Count<5);
         }
     }
 }
--- BattleShipLiteLibrary/GameLogic.cs
+++ BattleShipLiteLibrary/GameLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BattleShipLiteLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,57 @@ using System.Threading.Tasks;
 
 namespace BattleShipLiteLibrary
 {
-    class GameLogic
+   public class GameLogic
     {
+       public static void InitilizerGrid(PlayerInfoModel model)
+        {
+            List<string> letters = new List<string>
+            //letters.Add("A");
+            //letters.Add("B");
+            //letters.Add("C");
+            //letters.Add("B");
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E"
+
+            };
+
+            List<int> numbers = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+
+            foreach(var letter in letters)
+            {
+                foreach(var number in numbers)
+                {
+                    AddGridSpot(letter,number,model);
+                }
+            }
+           
+               
+        }
+
+        private static void AddGridSpot(string letter, int number,PlayerInfoModel model)
+        {
+            GridSpotModel spot = new GridSpotModel();
+            spot.SpotLetter = letter;
+            spot.SpotNumber = number;
+            spot.Status = GridSpotStatus.Empty;
+
+            model.ShotGrid.Add(spot); // Adding spot object to a ShotGrid
+        }
+
+        public static bool StoreShip(PlayerInfoModel model, string letter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
